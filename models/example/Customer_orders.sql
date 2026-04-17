d525b2c67bb025af77a5e2a5fc009b1430447d3d
doundoubolaru@gmail.com <doundoubolaru@gmail.com>
2026-04-17 07:36:03 +0000

    filtre sur customer 47423

--- models/example/Customer_orders.sql
+++ models/example/Customer_orders.sql
@@ -3,12 +3,14 @@ with customer_tab as (
 ),
 orders as (
     select * from {{ref('Orders')}}
+    
 ),
 
 customer_order as (
 select a.id as customer_key, o_orderkey,o_orderstatus,o_orderdate,o_orderpriority, sum(o_totalprice) total_price
 from customer_tab a
 left outer join orders b on a.ID=b.o_custkey
+where a.id=47423
 group by all
 )
 
